8332a18f772cd8a0ec18f087ddc8b3ebcc393fa4
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-06-23 17:04:41 +0800

    Prettier

--- ultimate-hooks/src/App.jsx
+++ ultimate-hooks/src/App.jsx
@@ -11,7 +11,7 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
@@ -20,26 +20,22 @@ const useResource = (baseUrl) => {
 
   // init...
   useEffect(() => {
-    axios.get(baseUrl)
-      .then((res) => {
-        setResources(res.data)
-      })
+    axios.get(baseUrl).then((res) => {
+      setResources(res.data)
+    })
   }, [])
 
-
-  const create = async (resource) => {  
+  const create = async (resource) => {
     const response = await axios.post(baseUrl, resource)
     setResources(resources.concat(response.data))
     return response.data
   }
 
   const service = {
-    create
+    create,
   }
 
-  return [
-    resources, service
-  ]
+  return [resources, service]
 }
 
 const App = () => {
@@ -54,10 +50,10 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -67,15 +63,21 @@ const App = () => {
         <input {...content} />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
   )
 }
